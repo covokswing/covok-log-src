5db9c701029a0d84c3d2da2ba50660a570765128
zzazang <changho.cha@gmail.com>
2020-05-03 00:50:47 +0900

    theme change, cms is not supporting mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,66 +7,67 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    // Used for the title template on pages other than the index site
-    siteTitle: `cov.ok.swing.kr`,
-    // Default title of the page
-    siteTitleAlt: `코복스윙log`,
-    // Can be used for e.g. JSONLD
-    siteHeadline: `코복스윙log`,
-    // Will be used to generate absolute URLs for og:image etc.
-    siteUrl: `https://covokswing.netlify.app`,
-    // Used for SEO
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    // Will be set on the <html /> tag
-    siteLanguage: `ko`,
-    // Used for og:image and must be placed inside the `static` folder
-    siteImage: `/banner.jpg`,
-    // Twitter Handle
-    author: `@lekoarts_de`,
-    // Links displayed in the header on the right side
-    externalLinks: [
-      {
-        name: `Twitter`,
-        url: `https://twitter.com/lekoarts_de`,
-      },
-      {
-        name: `Instagram`,
-        url: `https://www.instagram.com/lekoarts.de/`,
-      },
-    ],
-    // Navigation links
-    navigation: [
-      {
-        title: `Blog`,
-        slug: `/blog`,
-      },
-    ],
+    title: `cov.ok.swing.kr`,
+    description: `코복스윙log`,
+    // change this to you website url
+    siteUrl: `https://covokswing.netlify.com/`, 
+    home: {
+      title: `한국스윙 OK`,
+      description: `한국 스윙댄스 씬은 여전히 코로나19에 대한 경각심을 늦추지 않아야 한다고 생각하고 있고, 이를 위해 다양한 노력을 기울이고 있습니다. **코복스윙** (cov.ok.swing) 블로그는 스윙씬에서 코로나19의 위협에 대응하기 위한 노력들, 여러 구성원들의 생각, 현황을 한데 모았습니다. :)`,
+    },
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
     {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        showLineNumbers: false,
-        postsPath: "src/posts",
-        pagesPath: "src/pages",
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `facebook`,
-            url: `https://www.facebook.com/covidswingkr`,
-          },
-        ],
+        name: `markdown-pages`,
+        path: `${__dirname}/_data`,
       },
     },
-  ]
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+          },
+        },
+        {
+          resolve: 'gatsby-remark-emojis',
+        }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it. replace with yours
+        trackingId: "UA-164743872-1",
+        head: true,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Delog GatbsyJS Starter`,
+        short_name: `Delog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#381696`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+      },
+    },
+    `gatsby-plugin-sass`, 
+    `gatsby-plugin-react-helmet`,
+    // `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-dark-mode',
+    // siteURL is a must for sitemap generation
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
+  ],
 }
